e35e07cb34c3caa68b46f21e059e8a7b5020e632
Yaroslav Starenkyi <ystarenkiy@gmail.com>
2022-11-15 19:50:46 +0200

    Update Task1 (with RegEx)

--- src/main/java/task1/Task1.java
+++ src/main/java/task1/Task1.java
@@ -4,65 +4,30 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class Task1 {
     public static void main(String[] args) {
         File file = new File("/Users/yaroslavstarenkij/Desktop/GoIT/GIT/HomeWorkModule10/src/main/java/task1/file.txt");
 
-        MyBufferingReading(file);
+        matchingPhoneNumbers(file);
 
     }
 
-    private static void MyBufferingReading(File file) {
+    private static void matchingPhoneNumbers(File file) {
         try {
             FileReader fr = new FileReader(file);
             BufferedReader bf = new BufferedReader(fr);
             while (bf.ready()) {
                 String line = bf.readLine();
-                comparingPhoneNumbersFormat(line);
-
+                if (Pattern.matches("[(]\\d{3}[)] \\d{3}-\\d{4}", line)
+                        || Pattern.matches("\\d{3}-\\d{3}-\\d{4}", line)) {
+                    System.out.println(line);
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void comparingPhoneNumbersFormat(String line) {
-        if (line.length() == 12) { // comparing First type of phone numbers
-            int digitCounter = 0;
-            int hyphen = 0;
-            char[] chars = line.toCharArray();
-            for (char aChar : chars) {
-                if (aChar == '-') {
-                    hyphen++;
-                }
-                if (Character.isDigit(aChar)) {
-                    digitCounter++;
-                }
-            }
-            if (digitCounter == 10 && hyphen == 2) {
-                System.out.println(line);
-            }
-
-        }
-
-        if (line.length() == 14) { // comparing Second type of phone numbers
-            int digitCounter = 0;
-            char[] chars = line.toCharArray();
-            for (char aChar : chars) {
-                if (Character.isDigit(aChar)) {
-                    digitCounter++;
-                }
-
-            }
-            if (digitCounter == 10 &&
-                    chars[0] == '(' &&
-                    chars[4] == ')' &&
-                    chars[5] == ' ' &&
-                    chars[9] == '-') {
-                System.out.println(line);
-            }
-
-        }
-    }
 }
